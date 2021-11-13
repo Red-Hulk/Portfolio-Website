6f086a724b874121c928aa35d514f30e473a6148
Tristan <tristanemilmeijer@hotmail.com>
2021-11-13 23:22:06 +0100

    Experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5">
+           <h1>experience</h1>
+           </div>
+           <div className="container experience-wrapper">
+               {/*  */}
+               <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum eaque impedit ratione delectus, veritatis accusamus, facere repellat quae tenetur quos modi accusantium eius et aperiam, quam tempore? Unde voluptatem culpa quidem. Excepturi, accusamus cumque necessitatibus autem eum nemo voluptate?</p>
+                    </div>
+               </div>
+                {/*  */}
+               <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2015-2016</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum eaque impedit ratione delectus, veritatis accusamus, facere repellat quae tenetur quos modi accusantium eius et aperiam, quam tempore? Unde voluptatem culpa quidem. Excepturi, accusamus cumque necessitatibus autem eum nemo voluptate?</p>
+                    </div>
+               </div>
+
+               <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2016-2017</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum eaque impedit ratione delectus, veritatis accusamus, facere repellat quae tenetur quos modi accusantium eius et aperiam, quam tempore? Unde voluptatem culpa quidem. Excepturi, accusamus cumque necessitatibus autem eum nemo voluptate?</p>
+                    </div>
+               </div>
+
+               <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2017-2018</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum eaque impedit ratione delectus, veritatis accusamus, facere repellat quae tenetur quos modi accusantium eius et aperiam, quam tempore? Unde voluptatem culpa quidem. Excepturi, accusamus cumque necessitatibus autem eum nemo voluptate?</p>
+                    </div>
+               </div>
+
+               <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2018-2019</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum eaque impedit ratione delectus, veritatis accusamus, facere repellat quae tenetur quos modi accusantium eius et aperiam, quam tempore? Unde voluptatem culpa quidem. Excepturi, accusamus cumque necessitatibus autem eum nemo voluptate?</p>
+                    </div>
+               </div>
+            </div> 
+        </div>
+    )
+}
+
+export default Experience;
